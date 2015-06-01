e823311793e0c50ab71512e758f4dbb5da51b659
sunshineplan <sunshineplan@gmail.com>
2015-06-01 17:24:34 +0800

    写菜单，晚上查重功能

--- filechecker.py
+++ filechecker.py
@@ -1,14 +1,42 @@
 #!/usr/bin/python3
+# coding:utf-8
 
-from iolib import file2list
-from iolib import list2file
-from checkfunction import chk_repeat
-from checkfunction import compare
+from checkfunction import chk_repeat#chk_repeat(filename) return list which is repeated with repeated times
+from checkfunction import compare#compare(file1,file2) return which is file1 larger than file2
+from iolib import printlist
 
-a=file2list('file1.txt')
-b=file2list('file2.txt')
-c=compare(a,b)
-print('结果相差',len(c),'条记录')
-list2file(c,'output.txt')
+def menu():
+    print('1. Check Repeat(Single File)')
+    print('2. File Compare(Two Files)')
+    print('Q. Quit')
+    return 0
 
+def main():
+    print('File Checker')
+    menu()
+    choice=''
+    while choice.lower()!='q':
+        choice=input('Please choose one function:')
+        if choice=='1':
+            result=chk_repeat('file1.txt')
+            if result==[]:
+                print('file1.txt has no repeat')
+            else:
+                print('file1.txt has repeated content:')
+                print(result)
+                printlist(result)
+        elif choice=='2':
+            result=compare('file1.txt','file2.txt')
+            if result==[]:
+                print('file1.txt is same as file2.txt')
+            else:
+                print('file2.txt missing some contents')
+                printlist(result)
+        else:
+            print('Wrong Choice! Please choose again!')
+            menu()
+            choice=input()            
+
+if __name__ == '__main__':
+    main()
 
