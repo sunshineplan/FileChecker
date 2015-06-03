6e963304d01abc62fc90d26a36cfb18c6bc988b2
sunshineplan <sunshineplan@gmail.com>
2015-06-03 10:20:04 +0800

    重写compare，以及部分优化

--- checkfunction.py
+++ checkfunction.py
@@ -7,27 +7,25 @@ from iolib import list2file
 def chk_repeat(filename):
     file=file2list(filename)
     repeat=[]
+    i=filename.rindex('.')
     for a in file:
         if file.count(a)!=1:
             repeat.append(str(a)+'重复了'+str(file.count(a))+'次')
     output=list(set(repeat))#删除重复值
     output.sort()
-    if output!=[]:
-        changed=list(set(file))#将去重后的文件输出，文件名在原来的基础上加"_changed"后缀
+    if output!=[]:#如有重复，将去重后文件输出，文件名加上"_changed"
+        changed=list(set(file))
         changed.sort()
-        changedfile=list2file(changed,filename[:filename.rindex('.')]+'_changed'+filename[filename.rindex('.'):])
+        changedfile=list2file(changed,filename[:i]+'_changed'+filename[i:])
     return output
 
 def compare(file1,file2):
-    print('file1要包含file2')
-    c1=0
-    c2=0
+    f1=file2list(file1)
+    f2=file2list(file2)
+    c=0
     result=[]
-    while c2<len(file2):
-        if file1[c1]==file2[c2]:
-            c1+=1
-            c2+=1
-        else:
-            result.append(file1[c1])
-            c1+=1
+    while c<len(f1):
+        if f1[c] not in f2:
+            result.append(f1[c])
+        c+=1
     return result
