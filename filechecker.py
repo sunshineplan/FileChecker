d6cf0081fe70261c4a48bb8ab51ce4aeeba387b5
sunshineplan <sunshineplan@gmail.com>
2015-06-03 10:49:55 +0800

    重写主程序compare流程

--- filechecker.py
+++ filechecker.py
@@ -19,23 +19,38 @@ def main():
     menu()
     choice=input('Please choose one function:')
     while choice.lower()!='q':
+        r1=[]
+        r2=[]
         if choice=='1':
-            result=chk_repeat('file1.txt')
-            if result==[]:
+            r1=chk_repeat('file1.txt')
+            if r1==[]:
                 print('file1.txt has no repeat')
                 pause()
             else:
                 print('file1.txt has repeated content:')
-                print('\n'.join(result))
+                print('\n'.join(r1))
                 pause()
         elif choice=='2':
-            result=compare('file1.txt','file2.txt')
-            if result==[]:
+            r1=compare('file1.txt','file2.txt')
+            r2=compare('file2.txt','file1.txt')
+            if r1+r2==[]:
                 print('file1.txt is same as file2.txt')
                 pause()
+            elif r1==[]:
+                print('file2.txt完全包含file1.txt')
+                print('file2.txt比file1.txt多以下内容：')
+                print('\n'.join(r2))
+            elif r2==[]:
+                print('file1.txt完全包含file2.txt')
+                print('file1.txt比file2.txt多以下内容：')
+                print('\n'.join(r1))
             else:
-                print('file2.txt missing some contents')
-                print('\n'.join(result))
+                print('两个文件互相有缺少内容')
+                print('file1.txt比file2.txt多以下内容：')
+                print('\n'.join(r1))
+                print('\n')
+                print('file2.txt比file1.txt多以下内容：')
+                print('\n'.join(r2))
                 pause()
         else:
             print('Wrong Choice! Please choose again!')
