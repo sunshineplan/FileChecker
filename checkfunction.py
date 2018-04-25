dd04bc1bb2014ad2072467240487208003830d58
sunshineplan <sunshineplan@gmail.com>
2018-04-25 16:05:42 +0800

    检查功能增加非文件模式

--- checkfunction.py
+++ checkfunction.py
@@ -4,17 +4,23 @@
 from iolib import file2list
 from iolib import sort_data
 from iolib import save_original
+from iolib import precheck
 from time import time
 
-def chk_repeat(file1,file2='',display_warning='on'):
+def chk_repeat(file1,file2='',data_type='file',display_warning='on'):
     start_time=time()
-    data1=file2list(file1)
-    if display_warning=='on':
-        save_original(file1,data1)
-    if file2!='':
-        data2=file2list(file2)
+    if data_type=='file':
+        data1=file2list(file1)
         if display_warning=='on':
-            save_original(file2,data2)
+            save_original(file1,data1)
+        if file2!='':
+            data2=file2list(file2)
+            if display_warning=='on':
+                save_original(file2,data2)
+    else:
+        data1=precheck(file1)
+        if file2!='':
+            data2=precheck(file2)
     result=[]
     if file2=='':
         tmp=''
@@ -54,16 +60,19 @@ def compare(file1,file2,data_type='file',display_warning='on'):
     elapsed_time=time()-start_time
     return result,elapsed_time
 
-def chk_consecutive(filename):
+def chk_consecutive(filename,data_type='file'):
     start_time=time()
-    data=file2list(filename)
+    if data_type=='file':
+        data=file2list(filename)
+    else:
+        data=filename
     int_list=sorted(list(map(int,data)))         #将字符数组转换成数字数组
     start=int_list[0]
     end=int_list[-1]
     full_list=list(range(start,end+1))
     result,_=compare(full_list,int_list,data_type='list')
     result=list(map(str,result))
-    if result!=[]:
+    if result!=[] and data_type=='file':
         str_list=list(map(str,int_list))
         save_original(filename,str_list)
     elapsed_time=time()-start_time
