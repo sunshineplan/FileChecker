31354502d292dcd0f6c5a49f85612828808331b9
sunshineplan <sunshineplan@users.noreply.github.com>
2018-06-26 13:54:01 +0800

    Webapp merge compare function button

--- webapp.py
+++ webapp.py
@@ -51,54 +51,56 @@ def sda():
                 display='off')
     elif type == 'rm_duplicates':
         result = remove_duplicates(data, data_type='list')
-    elif type == 'chk_comm':
-        r1, elapsed_time = compare(data1, data2, mode='comm', data_type='list')
-        if r1 == []:
-            result.append('Two data contain no common values.\n')
-            result.append('Duration for process: ' +
-                          str(round(elapsed_time, 3)) + ' sec.')
-        else:
-            result += print_result(
-                r1,
-                title1='Common values found between two data.',
-                elapsed_time=elapsed_time,
-                result2file='off',
-                display='off')
-    elif type == 'chk_diff':
-        r1, elapsed_time1 = compare(data1, data2, data_type='list')
-        r2, elapsed_time2 = compare(data2, data1, data_type='list')
-        elapsed_time = elapsed_time1 + elapsed_time2
-        if r1 + r2 == []:
-            result.append('Data1 is same as Data2.\n')
-            result.append('Duration for process: ' +
-                          str(round(elapsed_time, 3)) + ' sec.')
-        elif r1 == []:
-            result.append('Data2完全包含Data1。\n')
-            result += print_result(
-                r2,
-                title1='Data2比Data1多以下内容',
-                elapsed_time=elapsed_time,
-                result2file='off',
-                display='off')
-        elif r2 == []:
-            result.append('Data1完全包含Data2。\n')
-            result += print_result(
-                r1,
-                title1='Data1比Data2多以下内容',
-                elapsed_time=elapsed_time,
-                result2file='off',
-                display='off')
-        else:
-            result.append('两个数据有互相不一致内容。\n')
-            result += print_result(
-                r1, title1='Data1比Data2多以下内容', result2file='off', display='off')
-            result.append('')
-            result += print_result(
-                r2,
-                title1='Data2比Data1多以下内容',
-                elapsed_time=elapsed_time,
-                result2file='off',
-                display='off')
+    elif type == 'compare':
+        mode = request.form.get('mode')
+        if mode == 'comm':
+            r1, elapsed_time = compare(data1, data2, mode='comm', data_type='list')
+            if r1 == []:
+                result.append('Two data contain no common values.\n')
+                result.append('Duration for process: ' +
+                              str(round(elapsed_time, 3)) + ' sec.')
+            else:
+                result += print_result(
+                    r1,
+                    title1='Common values found between two data.',
+                    elapsed_time=elapsed_time,
+                    result2file='off',
+                    display='off')
+        elif mode == 'diff':
+            r1, elapsed_time1 = compare(data1, data2, data_type='list')
+            r2, elapsed_time2 = compare(data2, data1, data_type='list')
+            elapsed_time = elapsed_time1 + elapsed_time2
+            if r1 + r2 == []:
+                result.append('Data1 is same as Data2.\n')
+                result.append('Duration for process: ' +
+                              str(round(elapsed_time, 3)) + ' sec.')
+            elif r1 == []:
+                result.append('Data2完全包含Data1。\n')
+                result += print_result(
+                    r2,
+                    title1='Data2比Data1多以下内容',
+                    elapsed_time=elapsed_time,
+                    result2file='off',
+                    display='off')
+            elif r2 == []:
+                result.append('Data1完全包含Data2。\n')
+                result += print_result(
+                    r1,
+                    title1='Data1比Data2多以下内容',
+                    elapsed_time=elapsed_time,
+                    result2file='off',
+                    display='off')
+            else:
+                result.append('两个数据有互相不一致内容。\n')
+                result += print_result(
+                    r1, title1='Data1比Data2多以下内容', result2file='off', display='off')
+                result.append('')
+                result += print_result(
+                    r2,
+                    title1='Data2比Data1多以下内容',
+                    elapsed_time=elapsed_time,
+                    result2file='off',
+                    display='off')
     elif type == 'chk_consecutive':
         try:
             r1, elapsed_time1 = chk_consecutive(data, data_type='list')
